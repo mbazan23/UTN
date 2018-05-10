1cb06960a8ae97f0f187e0d1fd8f641b65b12e54
Marcos Bazan <marcos.bazan.x@gmail.com>
2018-05-09 22:12:43 -0300

    Se arreglo un poquito el tema de funciones compuestas
    incPC listo, falta aplicarlo en todas las funciones todavia...

--- PDP/TP1.hs
+++ PDP/TP1.hs
@@ -1,51 +1,53 @@
 main = print $ "Hello, world!"
-data micro = micro{
+data Micro = Micro{
     memoriaDeDatos:: [Int],
     acumuladores::(Int, Int),
     programCounter::Int,
     etiquetaError::[Char]
 }deriving (Show)
 
-xt8088 = micro [] (0,0) 0 ""
-fp20 = micro [] (7,24) 0 ""
-at8086 = micro [1..20] (0,0) 0 ""
+xt8088 = Micro [] (0,0) 0 ""
+fp20 = Micro [] (7,24) 0 ""
+at8086 = Micro [1..20] (0,0) 0 ""
 
-nop micro = micro {
-	programCounter= (programCounter micro)+1
+nop micro = incPC micro
+
+incPC micro = micro {
+	programCounter = programCounter micro + 1
 }
 
-add micro = micro {
-	programCounter= (programCounter micro)+1,
+add micro = incPC (micro {
+	
     acumuladores=((fst.acumuladores) micro + (snd.acumuladores) micro,0)
-}
+})
  
-divide (micro memoriaDeDatos (a,0) programCounter etiquetaError) = micro memoriaDeDatos (a,0) (programCounter+1) "Division by zero"
-divide micro = micro {
-   acumuladores=((fst.acumuladores) micro `div` (snd.acumuladores) micro, 0),
-   programCounter = (programCounter micro ) + 1
-}
+divide (Micro memoriaDeDatos (a,0) programCounter etiquetaError) = Micro memoriaDeDatos (a,0) (programCounter+1) "Division by zero"
+divide micro = incPC ( micro {
+   acumuladores=((fst.acumuladores) micro `div` (snd.acumuladores) micro, 0)
+} )
 
-swap micro = micro (memoriaDeDatos micro) (snd (acumuladores micro),fst(acumuladores micro)) (programCounter micro+1) (etiquetaError micro)
+swap micro = incPC (micro {
+   acumuladores = ( snd(acumuladores micro) , fst (acumuladores micro) ) 
+})  
 
 esMayorALongitud posicion lista = (posicion > (length lista))
 
-lod addr (micro [] (a,b) programCounter etiquetaError) = micro [] (0,b) (programCounter+1) etiquetaError
-lod addr (micro memoriaDeDatos acumuladores programCounter etiquetaError) 
-    |(esMayorALongitud addr memoriaDeDatos)=micro memoriaDeDatos acumuladores (programCounter+1) "Tamanio mayor longitud de cadena"
-    |otherwise=micro memoriaDeDatos (memoriaDeDatos!!(addr-1), (snd acumuladores)) (programCounter+1) etiquetaError
+lod addr (Micro [] (a,b) programCounter etiquetaError) = Micro [] (0,b) (programCounter+1) etiquetaError
+lod addr (Micro memoriaDeDatos acumuladores programCounter etiquetaError) 
+    |(esMayorALongitud addr memoriaDeDatos)=Micro memoriaDeDatos acumuladores (programCounter+1) "Tamanio mayor longitud de cadena"
+    |otherwise=Micro memoriaDeDatos (memoriaDeDatos!!(addr-1), (snd acumuladores)) (programCounter+1) etiquetaError
 
-str addr val (micro memoriaDeDatos acumuladores programCounter etiquetaError) 
-    |esMayorALongitud (addr-1) memoriaDeDatos=micro memoriaDeDatos acumuladores (programCounter+1) "Tamanio mayor longitud de cadena"
-    |otherwise=micro (insertarElementoEnPosicion val memoriaDeDatos addr) acumuladores (programCounter+1) etiquetaError
+str addr val (Micro memoriaDeDatos acumuladores programCounter etiquetaError) 
+    |esMayorALongitud (addr-1) memoriaDeDatos=Micro memoriaDeDatos acumuladores (programCounter+1) "Tamanio mayor longitud de cadena"
+    |otherwise=Micro (insertarElementoEnPosicion val memoriaDeDatos addr) acumuladores (programCounter+1) etiquetaError
 
 insertarElementoEnPosicion x []     1 = x:[]
 insertarElementoEnPosicion x ys     1 = x:(tail ys)
 insertarElementoEnPosicion x (y:ys) n = y:insertarElementoEnPosicion x ys (n-1)
 
-lodv val micro = micro{
-	acumuladores=(val, (snd.acumuladores) micro), 
-	programCounter = (programCounter micro) + 1
-}
+lodv val micro = incPC (micro{
+	acumuladores=(val, (snd.acumuladores) micro)
+})
 
 punto2 xt8088 = (nop.nop.nop) xt8088
 
